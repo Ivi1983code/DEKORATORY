5cffb6b0160eb40197574ce7db91f4d89756330a
git config --global user.name "Iveta Szaboova <git config --global user.email "ivetka.szaboova@gmail.com>
2024-11-26 19:35:49 +0100

    casovy vstup

---
+++ operacie so subormi.py
@@ -0,0 +1,19 @@
+import time
+
+class TimeMeasurment:
+    def __enter__(self):
+        self.zaciatok = time.time()
+        self.cas_zaciatku = time.strftime("%H:%M:%S")  # Čas
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.koniec = time.time()
+        trvanie = int(self.koniec - self.zaciatok)
+        print(f"Dokončené v čase {self.cas_zaciatku} - blok trval {trvanie} sekundy")
+
+
+with TimeMeasurment() as t:
+    cislo = 1
+    for i in range(100000000):
+        cislo += i
+    print(cislo)
